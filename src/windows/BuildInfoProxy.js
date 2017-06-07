177946301d9e56e781b8fefed30de639642d22a7
bhayles <brucehayles@gmail.com>
2017-06-06 22:09:27 -0600

    Restoring lost changes (I don't know git) to BuildInfoProxy

--- src/windows/BuildInfoProxy.js
+++ src/windows/BuildInfoProxy.js
@@ -1,8 +1,17 @@
 BuildInfoProxy = {
 	init: function(successCallback, errorCallback, args) {
-		var result = {};
-		result.debug = Debug.DebugHelper.isDebug();
-		successCallback(result);
+        var package = Windows.ApplicationModel.Package.current;
+        var version = package.id.version;
+        var buildInfo = {
+            name: package.displayName,
+            displayName: package.displayName,
+            debug: Debug.DebugPlugin.isDebug(),
+            version: '%s.%s.%s.%s'.fmt(version.major, version.minor, version.build, version.revision),
+            packageName: package.id.name,
+            buildType: Debug.DebugPlugin.isDebug() ? "debug" : "release",
+	    };
+
+		successCallback(buildInfo);
 	}
 }
 
